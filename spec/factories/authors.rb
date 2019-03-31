cb217a67b817dec3e2de04a0e96654eb01bc4538
furuhama <furuhama@users.noreply.github.com>
2019-03-31 13:43:41 +0900

    Remove factories

--- spec/factories/authors.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :author do
-    name { 'Haruki Murakami' }
-    age { 13 }
-  end
-end
--- spec/factories/books.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :book do
-    author
-    title { 'Hitsuji Wo Meguru Bouken' }
-    pages { 200 }
-  end
-end
