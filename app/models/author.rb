3788ba0264698155dda25112482baeea815be51a
furuhama <furuhama@users.noreply.github.com>
2019-03-31 13:48:21 +0900

    Define Author#greet

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,12 @@ class Author < ApplicationRecord
   has_many :books
 
   validates :name, presence: true
+
+  def greet
+    if age
+      "Hi, my name is #{} and I am #{age} years old."
+    else
+      "Hi, my name is #{}."
+    end
+  end
 end
