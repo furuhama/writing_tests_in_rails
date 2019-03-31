0ae7159b7b1b7b3ef93f5ec0d61b173791f9f251
furuhama <furuhama@users.noreply.github.com>
2019-03-31 13:43:55 +0900

    Remove factory_bot settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,16 +5,10 @@ exit if ENV['BUNDLE_UPDATE']
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rspec/rails'
-require 'factory_bot'
 
 RSpec.configure do |config|
   config.order = :random
   config.render_views
-  config.include FactoryBot::Syntax::Methods
-
-  config.before(:all) do
-    FactoryBot.reload # これがないとfactoryの変更が反映されません
-  end
 
   config.before(:suite) do
     DatabaseRewinder[:active_record, connection: 'test']
