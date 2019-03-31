58bf805e5959a27a84a62b52ddb58203d7d93524
furuhama <furuhama@users.noreply.github.com>
2019-03-31 14:09:07 +0900

    Update spec

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -4,15 +4,17 @@ require 'spec_helper'
 
 RSpec.describe Book, type: :model do
   describe '#info' do
-    context 'page が nil の場合' do
-      let(:author) { Author.create!(name: 'Haruki Murakami') }
-      let(:book) { Book.create!(:book, author: author, title: '', page: nil) }
+    context 'title がある場合' do
+      context 'pages が nil の場合' do
+        let(:author) { Author.create!(name: 'Haruki Murakami') }
+        let(:book) { Book.create!(author: author, title: 'Hitsuji Wo Meguru Bouken', pages: nil) }
 
-      it { expect(book.info).to eq '' }
-    end
+        it { expect(book.info).to eq 'Hitsuji Wo Meguru Bouken: by Haruki Murakami' }
+      end
 
-    context 'page が 150 の場合' do
-      # ここを埋めてみよう
+      context 'pages が 150 の場合' do
+        # ここを埋めてみよう
+      end
     end
 
     context 'title が nil の場合' do
