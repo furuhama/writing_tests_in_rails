e45c0077bf3b6a33ca0ca20660930b12848937ba
furuhama <furuhama@users.noreply.github.com>
2019-03-31 13:36:05 +0900

    Add specs

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -3,4 +3,13 @@
 require 'spec_helper'
 
 RSpec.describe Book, type: :model do
+  describe 'FactoryBot' do
+    let(:book) { create(:book) }
+
+    it { expect(book.title).to eq 'Hitsuji Wo Meguru Bouken' }
+  end
+
+  describe 'DatabaseRewinder' do
+    it { expect(Book.count).to eq 0 }
+  end
 end
